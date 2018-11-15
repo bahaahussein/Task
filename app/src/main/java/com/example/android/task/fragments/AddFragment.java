3198d44abae0de1c67559f3a3951558171037e86
Bahaa Hussein <bahaauwk@gmail.com>
2018-11-15 20:18:51 +0200

    Add hint on the input field

--- app/src/main/java/com/example/android/task/fragments/AddFragment.java
+++ app/src/main/java/com/example/android/task/fragments/AddFragment.java
@@ -3,12 +3,16 @@ package com.example.android.task.fragments;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -26,6 +30,7 @@ public class AddFragment extends Fragment {
     private EditText editText;
     private Button button;
     private AddViewModel viewModel;
+    private TextInputLayout inputLayout;
 
     @Nullable
     @Override
@@ -33,10 +38,9 @@ public class AddFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
         editText = view.findViewById(R.id.edit_text_add);
         button = view.findViewById(R.id.button_add);
+        inputLayout = view.findViewById(R.id.input_addd);
         button.setOnClickListener((v) -> {
-            if(editText==null || editText.length()<=0)
-                Toast.makeText(getContext(), "Missing fields", Toast.LENGTH_SHORT).show();
-            else {
+            if(validate()) {
                 viewModel.addTask(new Task(editText.getText().toString()));
                 getFragmentManager().popBackStack();
             }
@@ -48,5 +52,47 @@ public class AddFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         viewModel = ViewModelProviders.of(this).get(AddViewModel.class);
         super.onActivityCreated(savedInstanceState);
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private boolean validate() {
+        if(editText==null || editText.getText().toString().trim().isEmpty()) {
+            inputLayout.setError("please enter a task");
+            requestFocus(editText);
+            return false;
+        }
+        inputLayout.setErrorEnabled(false);
+        return true;
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if(view.getId()==R.id.input_addd) {
+                validate();
+            }
+        }
     }
 }
